aa4a771e4a4d21ca569b306a1978fb852be71d3c
jankubin <101505317+jankubin@users.noreply.github.com>
2022-08-24 18:24:36 +0200

    jankubin
    edit

---
+++ Poznáváme Noty/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Poznáváme Noty
+//
+//  Created by Jan Kubín on 07.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func nextButton(_ sender: Any) {
+        let notyArray = [UIImage(named: "celaNota"), UIImage(named: "pulovaNota"), UIImage(named: "ctvrtovaNota"), UIImage(named: "osminovaNota"), UIImage(named: "sestnactinovaNota"), UIImage(named: "dvaatricetinovaNota"), UIImage(named: "pulovaSTeckouNota"), UIImage(named: "ctvrtovaSTeckouNota")]
+        
+        imageView.image = notyArray[Int.random(in: 0...7)]
+        
+    }
+    
+    @IBAction func celaNotaButton(_ sender: Any) {
+    }
+    
+    @IBAction func pulovaNotaButton(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
