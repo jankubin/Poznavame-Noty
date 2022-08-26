d56f7e76b429e6b87036fdbe71cb3ef060b0718e
jankubin <101505317+jankubin@users.noreply.github.com>
2022-08-26 12:17:53 +0200

    jankubin

--- Poznáváme Noty/ViewController.swift
+++ Poznáváme Noty/ViewController.swift
@@ -12,9 +12,12 @@ class ViewController: UIViewController {
     let notyArray = [UIImage(named: "celaNota"), UIImage(named: "pulovaNota"), UIImage(named: "ctvrtovaNota"), UIImage(named: "osminovaNota"), UIImage(named: "sestnactinovaNota"), UIImage(named: "dvaatricetinovaNota"), UIImage(named: "pulovaSTeckouNota"), UIImage(named: "ctvrtovaSTeckouNota")]
    
     @IBAction func nextButton(_ sender: Any) {
+        generateNext()
+    }
+    
+    func generateNext() {
         correctAnswer = Int.random(in: 0...7)
         imageView.image = notyArray[correctAnswer]
-        
     }
     
     
@@ -86,6 +89,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        generateNext()
         // Do any additional setup after loading the view.
     }
 
