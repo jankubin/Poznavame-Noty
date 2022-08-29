6582b6161dcce357157baf4fbda182058967c54f
jankubin <101505317+jankubin@users.noreply.github.com>
2022-08-29 12:49:07 +0200

    jankubin

---
+++ Poznáváme Noty/beckaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  beckaViewController.swift
+//  Poznáváme Noty
+//
+//  Created by Jan Kubín on 29.08.2022.
+//
+
+import UIKit
+
+class beckaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
