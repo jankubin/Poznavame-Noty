381d3c5acd9ebe2bb28ab92d9798e182be32ea59
jankubin <101505317+jankubin@users.noreply.github.com>
2022-08-29 11:25:16 +0200

    jankubin

--- Poznáváme Noty/NotyBasovyKlicProcvicovaniViewController.swift
+++ Poznáváme Noty/NotyBasovyKlicProcvicovaniViewController.swift
@@ -11,6 +11,9 @@ class NotyBasovyKlicProcvicovaniViewController: UIViewController {
     var correctAnswer = 1
     let notyArray = [UIImage(named: "h"), UIImage(named: "c1"), UIImage(named: "d1"), UIImage(named: "e1"), UIImage(named: "f1"), UIImage(named: "g1"), UIImage(named: "a1"), UIImage(named: "h1"), UIImage(named: "c2"), UIImage(named: "d2"), UIImage(named: "e2"), UIImage(named: "f2"), UIImage(named: "g2"), UIImage(named: "a2"), UIImage(named: "h2")]
     
+    @IBAction func nextButton(_ sender: Any) {
+        generateNext()
+    }
     
     
     
@@ -20,9 +23,134 @@ class NotyBasovyKlicProcvicovaniViewController: UIViewController {
         allButtons.forEach
         {$0.backgroundColor = .white}
     }
-
+    
+    @IBAction func velkeDKeyButton(_ sender: UIButton) {
+        if correctAnswer == sender.tag {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+    }
+    
+    @IBAction func velkeEKeyButton(_ sender: UIButton) {
+        if correctAnswer == sender.tag {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+    }
+    
+    @IBAction func velkeFKeyButton(_ sender: UIButton) {
+        if correctAnswer == sender.tag {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+    }
+    
+    @IBAction func velkeGKeyButton(_ sender: UIButton) {
+        if correctAnswer == sender.tag {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+    }
+    
+    @IBAction func velkeAKeyButton(_ sender: UIButton) {
+        if correctAnswer == sender.tag {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+    }
+    
+    @IBAction func velkeHKeyButton(_ sender: UIButton) {
+        if correctAnswer == sender.tag {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+    }
+    
+    @IBAction func cKeyButton(_ sender: UIButton) {
+        if correctAnswer == sender.tag {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+    }
+    
+    @IBAction func dKeyButton(_ sender: UIButton) {
+        if correctAnswer == sender.tag {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+    }
+    
+    @IBAction func eKeyButton(_ sender: UIButton) {
+        if correctAnswer == sender.tag {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+    }
+    
+    @IBAction func fKeyButton(_ sender: UIButton) {
+        if correctAnswer == sender.tag {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+    }
+    
+    @IBAction func gKeyButton(_ sender: UIButton) {
+        if correctAnswer == sender.tag {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+    }
+    
+    @IBAction func aKeyButton(_ sender: UIButton) {
+        if correctAnswer == sender.tag {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+    }
+    
+    @IBAction func hKeyButton(_ sender: UIButton) {
+        if correctAnswer == sender.tag {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+    }
+    @IBAction func c1KeyButton(_ sender: UIButton) {
+        if correctAnswer == sender.tag {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+    }
+    
+    @IBAction func d1KeyButton(_ sender: UIButton) {
+        if correctAnswer == sender.tag {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+    }
+    
+    
+    @IBOutlet var allButtons: [UIButton]!
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        generateNext()
 
         // Do any additional setup after loading the view.
     }
