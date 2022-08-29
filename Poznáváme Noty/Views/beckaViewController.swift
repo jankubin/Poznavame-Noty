6ac1bea1fecca56a5e5abd7728bcd0380207c169
jankubin <101505317+jankubin@users.noreply.github.com>
2022-08-29 14:45:20 +0200

    jankubin

--- Poznáváme Noty/Views/beckaViewController.swift
+++ Poznáváme Noty/Views/beckaViewController.swift
@@ -8,7 +8,46 @@
 import UIKit
 
 class beckaViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var popisTxt: UILabel!
+    
+    @IBAction func bb(_ sender: Any) {
+        imageView.image = UIImage(named: "bb")
+        popisTxt.text = "Toto je nota hes. Snížená nota h, ale nazýváme jí nota be."
+    }
+    
+    @IBAction func eb(_ sender: Any) {
+        imageView.image = UIImage(named: "eb")
+        popisTxt.text = "Toto je nota es. Snížená nota e."
+    }
+    
+    @IBAction func ab(_ sender: Any) {
+        imageView.image = UIImage(named: "ab")
+        popisTxt.text = "Toto je nota as. Snížená nota a."
+    }
+    
+    @IBAction func db(_ sender: Any) {
+        imageView.image = UIImage(named: "db")
+        popisTxt.text = "Toto je nota des. Snížená nota d."
+    }
+    
+    @IBAction func gb(_ sender: Any) {
+        imageView.image = UIImage(named: "gb")
+        popisTxt.text = "Toto je nota ges. Snížená nota g."
+    }
+    
+    @IBAction func cb(_ sender: Any) {
+        imageView.image = UIImage(named: "cb")
+        popisTxt.text = "Toto je nota ces. Snížená nota c."
+    }
+    
+    @IBAction func fb(_ sender: Any) {
+        imageView.image = UIImage(named: "fb")
+        popisTxt.text = "Toto je nota fes. Snížená nota f."
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- Poznáváme Noty/Views/krizkyViewController.swift
+++ Poznáváme Noty/Views/krizkyViewController.swift
@@ -8,7 +8,48 @@
 import UIKit
 
 class krizkyViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var popisTxt: UILabel!
+    
+    
+    @IBAction func fSharpButton(_ sender: Any) {
+        imageView.image = UIImage(named: "f#")
+        popisTxt.text = "Toto je nota fis. Zvýšená nota f."
+    }
+    
+    @IBAction func cSharpButton(_ sender: Any) {
+        imageView.image = UIImage(named: "c#")
+        popisTxt.text = "Toto je nota cis. Zvýšená nota c."
+    }
+    
+    @IBAction func gSharpButton(_ sender: Any) {
+        imageView.image = UIImage(named: "g#")
+        popisTxt.text = "Toto je nota gis. Zvýšená nota g."
+    }
+    
+    @IBAction func dSharpButton(_ sender: Any) {
+        imageView.image = UIImage(named: "d#")
+        popisTxt.text = "Toto je nota dis. Zvýšená nota d."
+    }
+    
+    @IBAction func aSharpButton(_ sender: Any) {
+        imageView.image = UIImage(named: "a#")
+        popisTxt.text = "Toto je nota ais. Zvýšená nota a."
+    }
+    
+    @IBAction func eSharpButton(_ sender: Any) {
+        imageView.image = UIImage(named: "e#")
+        popisTxt.text = "Toto je nota eis. Zvýšená nota e."
+    }
+    
+    @IBAction func hSharpButton(_ sender: Any) {
+        imageView.image = UIImage(named: "h#")
+        popisTxt.text = "Toto je nota his. Zvýšená nota h."
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
